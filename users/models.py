1efa2ad2ce8a9fe34b8a8447894eb256bf196930
Vamshedhar Reddy <vamshedhar@gmail.com>
2014-06-06 23:07:20 +0530

    feat(users): models created for users

--- users/models.py
+++ users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Users(models.Model):
+	username = models.CharField(max_length=20)
+	password = models.CharField(max_length=200)
+	firstName = models.CharField(max_length=100)
+	lastName = models.CharField(max_length=100)
+	email = models.EmailField(max_length=254)
+	contact = models.BigIntegerField()
+	balance = models.IntegerField(default=0)
+	timestamp = models.DateTimeField(auto_now_add=True)
