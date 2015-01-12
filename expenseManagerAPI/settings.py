7a48936bb19bc434ea6ffce48252560bfbee94db
Vamshedhar Reddy <vamshedhar@gmail.com>
2015-01-12 12:32:10 +0530

    :P

--- expenseManagerAPI/settings.py
+++ expenseManagerAPI/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=0bqc+c$c!g&3zylwu4+6r-20jjqi(tn)y9v2-hw^+zuid_5(2'
+SECRET_KEY = '=0bqc+c$c!g&3zylwu4+6r-20jjqi(tn)y9v2-hw^+zuid_5)2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
